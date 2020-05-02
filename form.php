c1141955656c8762145c795c6b82670154812689
I'm Josep <josepmariacampanya@gmail.com>
2020-05-02 13:05:39 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Promotourist - agencia de marketing y comunicación - Soluciones para marketing, comunicación y relaciones públicas - http://www.promotourist.com/">
+
+  <meta name="description" content="Jornadas Profesionales de Andalucía en Otoño del 2019">
+  <meta name="robots" content="index,follow">
+  <meta name="keywords" content="Evento, Andalucía, Malaga, Huelva, Jaén, Cordoba, Granada, Sevilla, Andalucía en Catalunya, Restaurantes, Autobuses, Hoteles, Turismo, MICE, sol y playa, rural, naturaleza, cultural, gastronómico, deportivo, salud, belleza, idiomas, NIEVE, sol y playa, rural, gastronomía">
+  <meta http-equiv="refresh" content="150">
+
+
+  <title>Jornadas Profesionales de Andalucía en Otoño del 2019</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <!--  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+
+  <!-- Plugin CSS -->
+  <!-- <link rel="stylesheet" href="device-mockups/device-mockups.min.css"> -->
+
+  <!-- Custom styles for this template -->
+  <link href="css/new-age.css" rel="stylesheet">
+
+
+
+
+
+</head>
+
+<body id="page-top">
+
+
+
+
+
+<!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN -->
+
+<section class="bg-inscripcion text-center" id="inscribete">
+
+    <div class="container">
+      <div class="section-heading text-center">
+        <h4 class="section-heading" style="color: #ffffff">Inscríbete</h4>
+      </div>
+    </div><!-- container -->
+
+        <!-- SPACE -->
+    <div class="container"><div style="height: 20px;"></div></div>
+    <!-- SPACE -->
+
+
+                        <div class="container">
+                        <div class="row">
+
+                        <div class="col-lg-12">
+                        <div class="section-heading text-center">
+                        <p class="text-intro">Si estás interesado en participar en el Workshop de Andalucía en Otoño cumplimenta este formulario y contactaremos contigo.</p>
+                        </div>
+                        </div> <!-- col-lg-12 -->
+
+                        </div><!-- row -->
+                        </div><!-- container -->
+
+                        <!-- SPACE -->
+                        <div class="container"><div style="height: 20px;"></div></div>
+                        <!-- SPACE -->
+
+                        <div class="container">
+                        <div class="row">
+
+                        <div class="col-lg-12">
+
+
+                        <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO -->  <!-- FORMULARIO -->  <!-- FORMULARIO --> 
+                        <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO -->  <!-- FORMULARIO -->  <!-- FORMULARIO --> 
+
+                        <form id="contact-form" method="post" action="contact.php" role="form">
+
+                        <div class="messages"></div>
+
+                        <div class="form-row">
+
+                        <div class="form-group col-md-6 text-left">
+                        <label class="text-form" for="form_name"><span style="font-size: 1.3em; color: #FFFF00;">*</span> Nombre de la Agencia</label>
+                        <input id="form_name" type="text" name="name" class="form-control" placeholder=" " required="required" data-error="Por favor, introduce el nombre de tu agencia">
+                        <div class="help-block with-errors"></div>
+                        </div>
+
+                        <div class="form-group col-md-6 text-left">
+                        <label class="text-form" for="form_lastname"><span style="font-size: 1.3em; color: #FFFF00;">*</span> Nombre y Apellidos del Agente que asistira</label>
+                        <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="" required="required" data-error="Por favor, introduce tu nombre y apellidos .">
+                        <div class="help-block with-errors"></div>
+                        </div>
+
+                        </div> <!-- form-row -->
+
+                        <div class="form-row">
+
+                        <div class="form-group col-md-6 text-left">
+                        <label class="form_email" for="inputAddress"><span style="font-size: 1.3em; color: #FFFF00;">*</span> Email</label>
+                        <input id="form_email" type="email" name="email" class="form-control" placeholder="" required="required" data-error="introduce tu email correctamente.">
+                        <div class="help-block with-errors"></div>
+                        </div>
+
+                        <div class="form-group col-md-6 text-left">
+                        <label class="form_phone" for="inputCity"><span style="font-size: 1.3em; color: #FFFF00;">*</span> Teléfono</label>
+                        <input id="form_phone" type="phone" name="phone" class="form-control" placeholder="" required="required" data-error="Por favor, introduce tu teléfono.">
+                        <div class="help-block with-errors"></div>
+                        </div>
+
+                        </div> <!-- form-row -->
+
+
+
+
+                        <div class="form-row">
+
+                        <div class="form-group col-md-6 text-left">
+                        <label class="text-form" for="form_need"><span style="font-size: 1.3em; color: #FFFF00;">*</span> Selecciona el día</label>
+                        <select id="form_need" name="need" class="form-control" required="required" data-error="Por favor, especifica el día que quieres asistir "> class="form-control">
+                        <option value="" selected>Elige el día del evento:</option>
+                        <option value="Lunes 23 de Septiembre - A Coruña">Lunes 23 de Septiembre - A Coruña</option>
+                        <option value="Martes 24 de Septiembre - Vigo">Martes 24 de Septiembre - Vigo</option>
+                        <option value="Miercoles 25 de Septiembre - Oporto">Miercoles 25 de Septiembre - Oporto</option>
+                        <option value="Jueves 26 de Septiembre - Lisboa">Jueves 26 de Septiembre - Lisboa</option>
+                        </select>
+                        <div class="help-block with-errors"></div>
+                        </div>
+
+                        <div class="form-group col-md-6 text-left">
+                        <label class="text-form" for="form_message"><span style="font-size: 1.3em; color: #FFFF00;"></span>Comentarios</label>
+                        <textarea id="form_message" name="message" class="form-control" placeholder="" rows="3"></textarea>
+                        <div class="help-block with-errors"></div>
+                        </div>
+
+
+                        <!-- CAPTCHA -->
+                        <div style="float:left;">
+                        Please type the code shown in the image: <input type="text" name="wsp_code"/>
+                        </div>
+                        <div style="float:left;">
+                        <script type="text/javascript" src="https://webspamprotect.com/captcha/7211/"></script>
+                        <noscript>This form protected by <a href="http://webspamprotect.com" target="_blank" title="Web form spam protection">WebSpamProtect</a>. JavaScript must be enabled in your browser to view this image.
+                        </noscript>
+                        </div>
+                        </div> <!-- CAPTCHA -->
+
+                        <div class="form-row">
+
+                        <div class="form-group col-md-6 text-left">
+                        <div class="section-heading text-left">
+                        <p class="text-intro-form" style="text-decoration: underline;">Importante:</p>
+                        <p class="text-intro-form">Una inscripción por asistente.</p>
+                        <p class="text-intro-form">Imprescindible rellenar todos los campos con (<span style="font-size: 1.3em; color: #FFFF00;">*</span>).</p>
+                        <p class="text-intro-form">Cumplimentar un formulario por asistente.</p>
+                        </div>
+                        </div> <!-- form-group col-md-6 text-left -->
+
+                        <div class="form-group col-md-6 text-left">
+                        <div class="form-group">
+                        <button type="submit" class="btn btn-outline btn-xl btn-enviar" value="Enviar solicitud">Enviar solicitud</button>
+
+                        </form>
+
+                        <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO -->  <!-- FORMULARIO -->  <!-- FORMULARIO --> 
+                        <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO --> <!-- FORMULARIO -->  <!-- FORMULARIO -->  <!-- FORMULARIO --> 
+
+                        </div>
+                        </div>
+
+                        </div> <!-- form-row -->
+                        </div> <!-- row -->
+                        </div><!-- container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+<!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN --> <!-- DONDE SE ORGANIZAN -->
+
+  <footer>
+    <div class="container">
+      <p class="text-form">&copy; Promotourist 2019. All Rights Reserved.</p>
+      <ul class="list-inline" style="display: inline-flex;">
+        
+        <li style="padding: 0% 5% 0% 5%;"><img src="img/logo_andalucia.png" width="100%"></li>
+        <li style="padding: 0% 5% 0% 5%;"><img src="img/logo_junta.png" width="100%"></li>
+        <li style="padding: 0% 5% 0% 5%;"><img src="img/logo_ue.png" width="100%"></li>
+
+      </ul>
+    </div>
+  </footer>
+
+
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/new-age.min.js"></script>
+
+
+
+
+
+  <!-- EMAIL DE CONTACTE -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js" integrity="sha256-dHf/YjH1A4tewEsKUSmNnV05DDbfGN3g7NMq86xgGh8=" crossorigin="anonymous"></script>
+
+<script src="contact.js"></script>
+  <!-- EMAIL DE CONTACTE -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145006509-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-145006509-1');
+  </script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+
+<?php
+include_once("wsp_captcha.php");
+
+if (WSP_CheckImageCode() != "OK") {
+    die("The image code you have entered is incorrect. Please, click the 'Back' button of your browser and type the correct one.");
+}
+?>
+
+
+</body>
+
+</html>
